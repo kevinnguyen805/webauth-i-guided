f176247299bd582ccbc7a0a821ecbc93cde4b96e
admin <admin@admins-MacBook-Pro.local>
2019-11-11 09:28:17 -0800

    bcrypt post hash password

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
+const configureMiddleware = require('./configure-middleware.js')
 
 const server = express();
+
+configureMiddleware(server)     // configureMiddleware file will take the server and configure all the middlewares for you
 
 server.use('/api', apiRouter);
 
